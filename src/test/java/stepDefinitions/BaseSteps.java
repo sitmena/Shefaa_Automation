8536fca8020444b9f47a996eb87ea2afb71b3f7b
hebafoudeh <heba.f@sitech.me>
2022-09-08 12:56:23 +0300

    Register new association

--- src/test/java/stepDefinitions/BaseSteps.java
+++ src/test/java/stepDefinitions/BaseSteps.java
@@ -13,21 +13,20 @@ public class BaseSteps {
     public TestContext testContext;
 
     public RegisterVolunteer registerVolunteer;
-<<<<<<< Updated upstream
 
-=======
+
+
     public LoginVolunteer loginVolunteer;
     public RegisterAssociation registerAssociation;
->>>>>>> Stashed changes
+
     public BaseSteps(TestContext context) {
         testContext = context;
         webDriver = testContext.getDriverManager().getDriver();
         registerVolunteer=new RegisterVolunteer(webDriver);
-<<<<<<< Updated upstream
-=======
+
         loginVolunteer=new LoginVolunteer(webDriver);
         registerAssociation=new RegisterAssociation(webDriver);
->>>>>>> Stashed changes
+
     }
 
 }
