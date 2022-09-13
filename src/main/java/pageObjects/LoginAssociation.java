ca770aadbacb3aa2bb9247046f54f790932670cd
hebafoudeh <heba.f@sitech.me>
2022-09-13 13:30:08 +0300

    Login already exiting association

--- src/main/java/pageObjects/LoginAssociation.java
+++ src/main/java/pageObjects/LoginAssociation.java
@@ -1,7 +1,11 @@
 package pageObjects;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.Wait;
 
 public class LoginAssociation {
 
@@ -11,7 +15,26 @@ public class LoginAssociation {
         this.webDriver = webDriver;
         PageFactory.initElements(webDriver, this);
     }
+    @FindBy(id = "mobilenum")
+    private WebElement mobileNum;
+    @FindBy(id = "pass")
+    private WebElement password;
+    @FindBy(id = "loginbtn")
+    private WebElement loginBtn;
+    @FindBy(id = "homeIcon")
+    private WebElement homeIcon;
 
 
+public void enterfields(int mobilenum,String pass){
+    this.mobileNum.sendKeys(""+mobilenum+"");
+    this.password.sendKeys(pass);
+}
 
+public void chooselogin(){
+    this.loginBtn.click();
+}
+    public void verifyuserunderhomepage(){
+        Wait.untilElementIsVisible(webDriver,homeIcon,20L);
+        Assert.assertTrue(homeIcon.isDisplayed());
+    }
 }
--- src/test/resources/features/LoginAssociation.feature
+++ src/test/resources/features/LoginAssociation.feature
@@ -7,7 +7,7 @@ Feature: Login Already Existing Association Tests
     Given user on be a friend of shefaa page And choose user type option <user type>
     When enter his mobile number And password <mobile number> <password>
     And choose login button
-    Then user will move to  association home page
+    Then user will move to association home page
     Examples:
-      | mobile number | password |
-      | 996279056368     | sitech@1234 |
+      |user type    | mobile number | password |
+      | association | 996279056368     | sitech@1234 |
